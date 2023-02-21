793b84ef4e0ab45231a04c37563b7ddbf9c9321e
RWNR <pacheung1@s.sfusd.edu>
2023-02-21 14:27:08 -0800

    lab 3 start

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 15.0f;
+    private Rigidbody playerRb;
+    public float horizontalInput;
+    public float xRange = -13;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput  * Time.deltaTime * speed);
+
+        // keep player inbound
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x < 13)
+        {
+            transform.position = new Vector3(13, transform.position.y, transform.position.z);
+        }
+    }
+}
